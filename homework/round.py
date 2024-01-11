8b48ceec94e978d0b49ff9b02b88a9ad1ada48bd
Dmitry <guzich_d@bk.ru>
2024-01-11 18:59:59 +0300

    clean_code_round

--- homework/round.py
+++ homework/round.py
@@ -1,15 +1,20 @@
 import math
 
+def check_point_in_round(x, y, r):
+    distance_to_origin = math.sqrt(x ** 2 + y ** 2)
+    # distance_to_origin = math.sqrt(x**2 + y**2 + z**2)
+
+    if distance_to_origin <= r:
+        return "Точка принадлежит кругу."
+    else:
+        return "Точка не принадлежит кругу."
+
+
 x = float(input("Введите координату x: "))
 y = float(input("Введите координату y: "))
 # z = float(input("Введите координату z: "))
 
 r = float(input("Введите радиус круга: "))
 
-distance_to_origin = math.sqrt(x**2 + y**2)
-# distance_to_origin = math.sqrt(x**2 + y**2 + z**2)
-
-if distance_to_origin <= r:
-    print("Точка принадлежит кругу.")
-else:
-    print("Точка не принадлежит кругу.")
+result = check_point_in_round(x, y, r)
+print(result)
